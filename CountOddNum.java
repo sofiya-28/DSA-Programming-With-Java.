cfa8f6d67eb21c1aef9b21301c987abd2a208593
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-19 19:09:27 +0530

    Count All Odd Num

--- CountOddNum.java
+++ CountOddNum.java
@@ -1,8 +1,8 @@
-public class CountEvenNum {
+public class CountOddNum{
     public static void main(String[] args){
         int[] arr ={1,2,3,4,5,6,7,8,9,10};
         for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==0)
+            if(arr[i]%2!=0)
                 System.out.println(arr[i]);
              
 
@@ -10,4 +10,7 @@ public class CountEvenNum {
 
     }
     
+
 }
+
+
