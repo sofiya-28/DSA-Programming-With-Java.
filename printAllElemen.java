2c730ec067f86fe42d2ebf01134d8f3b7b03b328
sofiya-28 <sofiyamalik91@gmail.com>
2026-06-19 17:49:04 +0530

    Print All Element Of Array

---
+++ printAllElemen.java
@@ -0,0 +1,9 @@
+public class printAllElemen {
+    public static void main(String[] args) {
+        int[] marks ={20,30,40,80,90,100};
+         for(int i=0;i<marks.length;i++){
+            System.out.print(marks[i]+" ");
+         }
+    }
+    
+}
