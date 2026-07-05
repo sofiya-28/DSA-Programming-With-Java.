38a9dbc4356aba525fa05865935d495522d8256f
sofiya-28 <sofiyamalik91@gmail.com>
2026-07-05 23:58:04 +0530

    Added max program

---
+++ ArrayMaxElement.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ArrayMaxElement{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        //array size
+        System.out.println("Enter the size of Array");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        //no of element in array
+        System.out.println("Enter the element of Array");
+        for(int i=0;i<n;i++){
+            arr[i] =sc.nextInt();
+            System.out.println(arr[i]);
+        }
+        // find maximum  num 
+        int max =arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max)
+                max=arr[i];
+            
+        }
+         System.out.println(max);
+
+    sc.close();
+    }
+
+}
